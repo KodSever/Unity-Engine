8051d184f8183b53906e993966700673236d6d0e
Yiğit KARAKOYUN <38832410+KodSever@users.noreply.github.com>
2019-05-30 21:18:01 +0300

    Update and rename çocuklarını alma.cs to çocuklarını bulma.cs

--- çocuklarını bulma.cs
+++ çocuklarını bulma.cs
@@ -1,13 +1,3 @@
-//ÇOCUKLARINI BULMA SADE
-
-    Transform[] allChildren = nesne1.GetComponentsInChildren<Transform>();
-    
-    foreach (Transform child in allChildren)
-    {
-        GameObject obj = child.gameObject;
-        Debug.Log(obj);
-    }
-
 //ÇOCUKLARINI BULMA KARMASIK
 
     #region ÇOCUKLAR

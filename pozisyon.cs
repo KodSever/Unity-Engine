a8218e3c8f063094cdf3392ad25cb12e4de39512
Yiğit KARAKOYUN <38832410+KodSever@users.noreply.github.com>
2019-05-30 21:15:04 +0300

    Update and rename pozisyon.md to pozisyon.cs

---
+++ pozisyon.cs
@@ -0,0 +1,4 @@
+// İLK DEĞERLERİ ALMAK
+  
+    yarimCemberVector = yarimCemberNesnesi.transform.localPosition;
+    ceyrekCemberVector = ceyrekCemberNesnesi.transform.localPosition;
--- pozisyon.md
+++
@@ -1,5 +0,0 @@
-# İLK DEĞERLERİ ALMAK
-  
-  yarimCemberVector = yarimCemberNesnesi.transform.localPosition;
-  
-  ceyrekCemberVector = ceyrekCemberNesnesi.transform.localPosition;
